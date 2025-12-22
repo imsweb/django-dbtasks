809f0846cd74056bebad2feec29a60ad8d6ad761
Dan Watson <dcwatson@gmail.com>
2025-12-22 09:07:25 -0500

    Bump version, add publish workflow

--- src/dbtasks/__init__.py
+++ src/dbtasks/__init__.py
@@ -1,5 +1,12 @@
+import importlib.metadata
+
 from .periodic import Periodic
 from .schedule import Crontab, Duration, Every, Schedule, ScheduleExhausted
+
+__version__ = importlib.metadata.version("django-dbtasks")
+__version_info__ = tuple(
+    int(num) if num.isdigit() else num for num in __version__.split(".")
+)
 
 __all__ = [
     "Crontab",
@@ -8,4 +15,6 @@ __all__ = [
     "Periodic",
     "Schedule",
     "ScheduleExhausted",
+    "__version__",
+    "__version_info__",
 ]
